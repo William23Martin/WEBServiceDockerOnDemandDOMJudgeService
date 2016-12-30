9e35d9bdb27a2a9ef3cbd6acd324ff8a4333013d
William Martín <willy23martin@gmail.com>
2016-12-30 13:20:20 -0500

    A

--- entregables/bdScripts/seleccionarUsuarios.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sql_host="localhost"
-slq_usuario="root"
-sql_password="password"
-sql_database="usuarios"
-sql_args="-h $sql_host -u $slq_usuario -p$sql_password -D $sql_database -s -e"
-mysql $sql_args "insert into usuario(nombreUsuario,nombreContenedor) values('usuarioN','contenedorN');"
